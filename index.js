2be9b5c2b94703232babe8cd054b6f74842e34d3
Nero Rodrigues <nero.rodrigues@gmail.com>
2018-11-15 01:38:43 -0200

    Fixing index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var http = require('http');
+const http = require('http');
 
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hi there!");
-
+var httpServer =  http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain'});
+    response.end('Hi There!');
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+var port = process.env.PORT || 8001;
+
+httpServer.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
